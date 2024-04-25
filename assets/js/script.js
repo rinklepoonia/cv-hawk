00b90352c25bef5d61b8f7ddacbbfe013b94ae09
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-25 11:24:57 +0530

    thems-section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,39 @@ function openNav() {
     document.body.classList.toggle("overFlowHidden");
     document.querySelector(".menuIcon").classList.toggle("cross")
 }
+// ========them-slider
+$('.theam_slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
