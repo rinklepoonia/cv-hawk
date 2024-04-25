1b6a18925daf2d456f32e6783412177eb8ff189d
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-25 10:33:20 +0530

    hero-section done

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overFlowHidden");
+    document.querySelector(".menuIcon").classList.toggle("cross")
+}
