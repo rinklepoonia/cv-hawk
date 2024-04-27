3a8563534be2593da04ceafd8a6580622fb108df
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-27 15:12:59 +0530

    remover border-bottom from last acc.item

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,4 +43,18 @@ $('.client_slider').slick({
     responsive: [
     ]
 });
+// ===============back-to-top
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 900) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+});
 
